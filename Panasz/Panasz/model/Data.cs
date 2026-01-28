705cd8b1a5caf2ce5a245b01fbf22b267173527b
bzalan78 <bokanyizalan@turr.hu>
2026-01-28 14:14:29 +0100

    kezdes

---
+++ Panasz/Panasz/model/Data.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panasz.model
+{
+    internal class Data
+    {
+        public int id { get; set; }
+        public string nev { get; set; }
+        public string email { get; set; }
+        public string telefon { get; set; }
+        public string panaszado { get; set; }
+        public string panasz { get; set; }
+        
+
+      
+    }
+}
