373c15faeb3c66c25a588a3ea6e1e637c2d11b80
bzalan78 <bokanyizalan@turr.hu>
2026-02-02 13:25:42 +0100

    program kezdes

--- Panasz/Panasz/Program.cs
+++ Panasz/Panasz/Program.cs
@@ -1,7 +1,24 @@
-﻿internal class Program
+﻿using AdatbazisTeszt.Database;
+using Panasz.model;
+using System.Data;
+
+internal class Program
 {
-    private static void Main(string[] args)
+    private static void Main(string[] args) { }
+    //{
+        //* public static readonly string connectionString
+      //  = "Server=localhost;Database=11a_foldrajz;User=root;";
+   // public static DataTable adatok = new DataTable();
+    //public static List<Orszag> orszagLista = new List<Orszag>();
+    private static void SelectFromTable(string tableName, string connectionString)
     {
-        
+       //adatok = DatabaseService.GetAllData(tableName, connectionString);
+        Console.WriteLine("Adatok sikeresen szinkronizálva az adatbázisból, átmeneti tárolóba");
     }
+
+    private static void DBCheck(string connectionString)
+    {
+        DatabaseService.DbConnectionCheck(connectionString);
+    }
+
 }
