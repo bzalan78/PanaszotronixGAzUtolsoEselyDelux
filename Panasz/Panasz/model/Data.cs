e926fd2ff43b81ad9c73f410c93de8744c1616f3
bzalan78 <bokanyizalan@turr.hu>
2026-02-02 12:55:49 +0100

    sqlconnector

--- Panasz/Panasz/model/Data.cs
+++ Panasz/Panasz/model/Data.cs
@@ -8,11 +8,23 @@ namespace Panasz.model
 {
     internal class Data
     {
+        public Data(int id, string nev, string email, string telefon, string panaszado, string datum, string panasz)
+        {
+            this.id = id;
+            this.nev = nev;
+            this.email = email;
+            this.telefon = telefon;
+            this.panaszado = panaszado;
+            this.datum = datum;
+            this.panasz = panasz;
+        }
+
         public int id { get; set; }
         public string nev { get; set; }
         public string email { get; set; }
         public string telefon { get; set; }
         public string panaszado { get; set; }
+        public string datum { get; set; }
         public string panasz { get; set; }
         
 
